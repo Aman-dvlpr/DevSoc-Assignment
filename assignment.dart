98dbdf0a3b28f07b00da1f97513ff60711565e1c
Aman_dvlpr <agarwal.aman0211@gmail.com>
2025-10-26 23:43:08 +0530

    First commit

--- assignment.dart
+++ assignment.dart
@@ -9,27 +9,59 @@
 
 // Assignment 1: Reverse a String
 // Input: "hello" -> Output: "olleh"
+import 'dart:developer';
+import 'dart:async';
+
 String reverseString(String str) {
-  return "";
+  int i=0;
+  String rev="";
+  for (i=(str.length-1); i>=0; i-- )
+  {
+    rev=rev+str[i];
+  }
+  return rev;
 }
 
 // Assignment 2: Find the Largest Number in a List
 // Input: [1, 5, 3, 9, 2] -> Output: 9
 int findLargestNumber(List<int> arr) {
-  return 0;
+  int max=0;
+  for( int num in arr)
+  {
+    if(num>max)
+    max=num;
+  }
+
+  return max;
 }
 
 // Assignment 3: Check for Palindrome
 // Input: "racecar" -> true
 // Input: "hello" -> false
 bool isPalindrome(String str) {
+  int i=0;
+  String rev="";
+   for (i=(str.length-1); i>=0; i-- )
+  {
+    rev=rev+str[i];
+  }
+  if(rev==str)
+  return true;
   return false;
 }
 
 // Assignment 4: Sum of Even Numbers in a List
 // Input: [1, 2, 3, 4, 5, 6] -> 12
 int sumEvenNumbers(List<int> arr) {
-  return 0;
+  int s=0;
+  for(int num in arr)
+  {
+    if(num%2==0)
+    {
+      s=s+num;
+    }
+  }
+  return s;
 }
 
 // Assignment 5: FizzBuzz
@@ -37,13 +69,45 @@ int sumEvenNumbers(List<int> arr) {
 // Input: 15
 // Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
 List<String> fizzBuzz(int n) {
-  return [];
+  List<String> Fizzbuzz=[];
+  for(int i=1; i<=n; i++)
+  {
+    if(i%3==0 && i%5==0)
+    Fizzbuzz.add("FizzBuzz");
+    else if(i%3==0)
+    {
+      Fizzbuzz.add("Fizz"); 
+
+    }
+    else if(i%5==0)
+    Fizzbuzz.add("Buzz");
+    else
+    {
+      Fizzbuzz.add(i.toString());
+    }
+    }
+
+
+  return Fizzbuzz;
 }
 
 // Assignment 6: Find Pair with Given Sum
 // Input: [1, 2, 3, 4, 5], 9 -> true
 // Input: [1, 2, 3, 4, 5], 10 -> false
 bool hasPairWithSum(List<int> arr, int target) {
+  arr.sort();
+  int l=0;
+  int r=arr.length-1;
+  while(l<r)
+  {
+    int sum=arr[l]+arr[r];
+    if(sum==target)
+    return true; 
+    else if(sum<target)
+    l++;
+    else
+    r--;
+  }
   return false;
 }
 
@@ -51,26 +115,50 @@ bool hasPairWithSum(List<int> arr, int target) {
 // Input: "hello" -> 2
 // Input: "apple" -> 2
 int countVowels(String str) {
-  return 0;
+  int c=0; 
+  for( int i=0; i<str.length; i++)
+  {
+    if("AEIOUaeiou".contains(str[i]))
+    {
+      c++;
+    }
+  }
+  return c;
 }
 
 // Assignment 8: Count Occurrences of an Element
 // Input: [1, 2, 3, 2, 4, 2], 2 -> 3
 int countOccurrences(List<int> arr, int target) {
-  return 0;
+  int c=0;
+  for(int num in arr)
+  {
+    if(num==target)
+    c++;
+  }
+  return c;
 }
 
 // Assignment 9: Find the First Even Number
 // Input: [1, 3, 5, 6, 7, 8] -> 6
 // Input: [1, 3, 5, 7] -> null
 int? findFirstEven(List<int> arr) {
+  for( int num in arr)
+  {
+    if(num%2==0)
+    return num;
+  }
   return null;
 }
 
 // Assignment 10: Factorial of a Number
 // Input: 5 -> 120
 int factorial(int n) {
-  return 0;
+  int fac=1;
+  for(int i=1; i<=n; i++)
+  {
+    fac=fac*i;
+  }
+  return fac;
 }
 
 // Assignment 11: Promise chain
@@ -79,9 +167,13 @@ int factorial(int n) {
 // Use this link: https://dart.dev/libraries/dart-async
 Future<int> asyncChain(int num) async {
   // step 1
-  await Future.delayed(Duration(milliseconds: 100)); // we're simulating an async request
+  await Future.delayed(Duration(milliseconds: 100));
+  num=num+10; // we're simulating an async request
   // step 2
   await Future.delayed(Duration(milliseconds: 100));
+  num=num*2;
   // step 3
-  return 0;
+  await Future.delayed(Duration(milliseconds: 100));
+  num=num-5;
+  return num;
 }
